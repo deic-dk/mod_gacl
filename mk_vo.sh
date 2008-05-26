04b143760e12963ad090e168bff576d94983455e
Frederik Orellana <frederik@orellana.dk>
2008-05-26 14:00:28 +0000

    First functional version.

--- mk_vo.sh
+++ mk_vo.sh
@@ -21,15 +21,20 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see http://www.gnu.org/licenses/.
 
+#
+# Configuration
+#
 
 # The DocumentRoot of the web server. When recursing upwards looking
 # for a .gacl file, this is where we stop. You may want to change this.
 DOCUMENT_ROOT="/var/www/html/grid/data"
-
 GACL_FILE=".gacl"
+GACL_VO_FILE=".gacl_vo"
 DN_LIST_URL_TAG="dn-list-url"
 
+#
 # First find the directory containing the .gacl file to check.
+#
 
 # The argument supplied to this script must be a path - either of a
 # directory or of a file. Here we find the directory (and the file name).
@@ -38,7 +43,7 @@ path="$@"
 
 if [ -z $path ]; then
   echo "no path given"
-  return -1
+  exit -1
 fi
 
 if [ -d $path ]; then
@@ -49,11 +54,11 @@ else
   file=`echo $path | sed -r 's|.*/([^/]+)|\1|'`
 fi
 
-# Recurse upwards until a .gacl file is found
+# Recurse upwards until a .gacl file is found.
 
 while [ $PWD != DOCUMENT_ROOT -a $PWD != "/" ]; do
   if [ -e $GACL_FILE ]; then
-    echo OK
+    echo "Found $GACL_FILE file in $PWD"
     break
   fi
   cd ..
@@ -61,37 +66,68 @@ done
 
 if [ ! -e $GACL_FILE ]; then
   echo "no $GACL_FILE file found"
-  return -2
+  exit -2
 fi
 
+#
 # Check if the .gacl file contains elements of the form
 # <dn-list-url>https://some.url/vo.txt</dn-list-url>.
+# If it does, construct list of URLs.
+#
 
 # Get the list of URLs
 gacl_line=`sed -n '1h;2,$H;${g;s/\n//g;p}' .gacl | sed -r 's/>\s+</></g'`
 old_line=""
-while [ "$gacl_line" != "$old_line" ]; do
-  old_line=$gacl_line
-  gacl_line=`echo "$old_line" | sed -r 's|(.*)<dn-list-url>(.*)</dn-list-url>(.*)|\2\\t\1\3|'`
+url_list=$gacl_line
+while [ "$url_list" != "$old_line" ]; do
+  old_line=$url_list
+  url_list=`echo "$old_line" | sed -r "s|(.*)<$DN_LIST_URL_TAG>(.*)</$DN_LIST_URL_TAG>(.*)|\2\\t\1\3|"`
 done
-url_list=`echo "$gacl_line" | sed -r 's|(.*http://\S+)\t[^\t]*|\1|'`
+url_list=`echo "$url_list" | sed -r 's|(.*http[s]*://\S+)\t[^\t]*|\1|'`
 
-if [ -z "$url_list" ]; then
+if [ -z "$url_list" -o "$url_list" == "$gacl_line" ]; then
   echo "no $DN_LIST_URL_TAG tag found"
-  return 0
+  exit 0
 fi
 
-## TODO
-# Get the corresponding list of entries
+#
+# Construct list of entries in the form
+# URL1<allow>...</allow><deny>...</deny>URL2<allow>...</allow><deny>...</deny>...
+#
+
 gacl_line=`sed -n '1h;2,$H;${g;s/\n//g;p}' .gacl | sed -r 's/>\s+</></g'`
 old_line=""
-while [ "$gacl_line" != "$old_line" ]; do
-  old_line=$gacl_line
-  gacl_line=`echo "$old_line" | sed -r 's|(.*)(<entry>.*)<dn-list-url>.*</dn-list-url>(.*</entry>)(.*)|\2\3\\t\1\4|'`
+entries_list=`echo "$gacl_line" | sed 's|<entry>|<entry\t>|gi' | sed 's|</entry>|</entry\t>|gi'`
+while [ "$entries_list" != "$old_line" ]; do
+  old_line=$entries_list
+  entries_list=`echo "$old_line" | sed -r "s|(.*)<entry\t>([^\t]*)<$DN_LIST_URL_TAG>([^\t]*)</$DN_LIST_URL_TAG>([^\t]*)</entry\t>(.*)|\3\2\4\1\5|"`
 done
-entries_list=`echo "$gacl_line" | sed -r 's|(.+)\t[^\t]*|\1|'`
+entries_list=`echo "$entries_list" | sed -r 's|(.+)<gacl>.*|\1|i'`https://
+#echo "$url_list" | tr '\t' ' '
+#echo "$entries_list"
 
-echo "$url_list" | tr '\t' ' '
-echo "$entries_list"
+#
+# Write the .gacl_vo file.
+#
 
-touch $dir/.gacl_vo
+echo "Writing file $GACL_VO_FILE"
+
+echo "<gacl>" > $GACL_VO_FILE
+for url in $url_list; do
+  url1=`echo $url | sed 's|http[s]*://|\t|g'`
+  perms=`echo $entries_list | sed 's|http[s]*://|\t|g'`
+  perms=`echo "$perms" | sed -r "s|.*$url1([^\t]+)\t.*|\1|"`
+  echo URL: "$url"
+  echo PERMS: "$perms"
+  curl --insecure $url 2>/dev/null | sed 's/\"//g' | while read name; do
+cat >> $GACL_VO_FILE <<EOF
+  <entry>
+    <person>
+      <dn>$name</dn>
+    </person>
+    $perms
+  </entry>
+EOF
+  done
+done
+echo "</gacl>" >> $GACL_VO_FILE
